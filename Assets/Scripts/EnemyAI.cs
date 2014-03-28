87da7fced0dc91575ac7d7f84c0b2cd13c005dc0
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-03-28 13:25:51 -0700

    Add very basic Enemy AI to game.
    Add very basic Enemy AI. Cube in house will chase player if player
    gets too close, and will stop chasing player if player gets far
    enough away.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	public float patrolSpeed = 2f;                          // The nav mesh agent's speed when patrolling.
+	public float chaseSpeed = 5f;                           // The nav mesh agent's speed when chasing.
+	public float chaseWaitTime = 5f;                        // The amount of time to wait when the last sighting is reached.
+	public float patrolWaitTime = 1f;                       // The amount of time to wait when the patrol way point is reached.
+	public Transform[] patrolWayPoints;                     // An array of transforms for the patrol route.
+	
+	
+	private EnemySight enemySight;                          // Reference to the EnemySight script.
+	private NavMeshAgent nav;                               // Reference to the nav mesh agent.
+	private Transform player;                               // Reference to the player's transform.
+	private float chaseTimer;                               // A timer for the chaseWaitTime.
+	private float patrolTimer;                              // A timer for the patrolWaitTime.
+	private int wayPointIndex;                              // A counter for the way point array.
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		enemySight = GetComponent<EnemySight>();
+		nav = GetComponent<NavMeshAgent>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	
+	void Update ()
+	{		
+		// If the player has been sighted and isn't dead...
+		if (enemySight.personalLastSighting != enemySight.resetPosition) 
+		{
+			// ... chase.
+			Chasing ();
+		}
+
+		// Otherwise...
+		else
+		{
+			// ... patrol.
+			Patrolling();
+		}
+	}	
+	
+	void Chasing ()
+	{
+		// Create a vector from the enemy to the last sighting of the player.
+		Vector3 sightingDeltaPos = enemySight.personalLastSighting - transform.position;
+		
+		// If the the last personal sighting of the player is not close...
+		if(sightingDeltaPos.sqrMagnitude > 4f)
+			// ... set the destination for the NavMeshAgent to the last personal sighting of the player.
+			nav.destination = enemySight.personalLastSighting;
+		
+		// Set the appropriate speed for the NavMeshAgent.
+		nav.speed = chaseSpeed;
+		
+		// If near the last personal sighting...
+		if(nav.remainingDistance < nav.stoppingDistance)
+		{
+			// ... increment the timer.
+			chaseTimer += Time.deltaTime;
+			
+			// If the timer exceeds the wait time...
+			if(chaseTimer >= chaseWaitTime)
+			{
+				// ... reset last global sighting, the last personal sighting and the timer.
+				enemySight.personalLastSighting = new Vector3(1000f, 1000f, 1000f);
+				chaseTimer = 0f;
+			}
+		}
+		else
+			// If not near the last sighting personal sighting of the player, reset the timer.
+			chaseTimer = 0f;
+	}
+	
+	
+	void Patrolling ()
+	{
+		nav.Stop();
+	}
+}
---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour 
+{
+	public float fieldOfViewAngle = 110f;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+
+	private NavMeshAgent nav;
+	private SphereCollider col;
+	private GameObject player;
+	private Vector3 previousSighting;
+
+	public Vector3 resetPosition = new Vector3(1000f, 1000f, 1000f);
+
+	void Awake() 
+	{
+		nav = GetComponent<NavMeshAgent> ();
+		col = GetComponent<SphereCollider> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		personalLastSighting = resetPosition;
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		// If the player has entered the trigger sphere...
+		if(other.gameObject == player)
+		{
+			playerInSight = true;
+			personalLastSighting = player.transform.position;
+		}
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		// If the player leaves the trigger zone...
+		if(other.gameObject == player)
+			// ... the player is not in sight.
+			playerInSight = false;
+	}
+	
+	
+	float CalculatePathLength (Vector3 targetPosition)
+	{
+		// Create a path and set it based on a target position.
+		NavMeshPath path = new NavMeshPath();
+		if(nav.enabled)
+			nav.CalculatePath(targetPosition, path);
+		
+		// Create an array of points which is the length of the number of corners in the path + 2.
+		Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
+		
+		// The first point is the enemy's position.
+		allWayPoints[0] = transform.position;
+		
+		// The last point is the target position.
+		allWayPoints[allWayPoints.Length - 1] = targetPosition;
+		
+		// The points inbetween are the corners of the path.
+		for(int i = 0; i < path.corners.Length; i++)
+		{
+			allWayPoints[i + 1] = path.corners[i];
+		}
+		
+		// Create a float to store the path length that is by default 0.
+		float pathLength = 0;
+		
+		// Increment the path length by an amount equal to the distance between each waypoint and the next.
+		for(int i = 0; i < allWayPoints.Length - 1; i++)
+		{
+			pathLength += Vector3.Distance(allWayPoints[i], allWayPoints[i + 1]);
+		}
+		
+		return pathLength;
+	}
+}
