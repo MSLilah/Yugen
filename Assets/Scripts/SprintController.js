9ee2aaacb4cb253b314949e60a614d0a627c3529
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-03-31 19:33:02 -0700

    Add sprinting
    Add ability to sprint when pressing the left shift key. Sprinting
    uses a stamina variable, and player cannot sprint when this
    value goes below zero. Value will recharge when character is
    sprinting. If stamina is emptied, player cannot sprint again until
    it becomes at least half full.

---
+++ Assets/Scripts/SprintController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var walkSpeed: float = 6; // regular speed
+var runSpeed: float = 20; // run speed
+private var chMotor: CharacterMotor;
+var stamina = 100f;
+var noSprint = false;
+
+function Start () {
+	chMotor = GetComponent(CharacterMotor);
+}
+
+function Update () {
+	if (chMotor.grounded && Input.GetKey("left shift") && (Input.GetKey("w") || Input.GetKey("up")) && !noSprint) {
+		chMotor.movement.maxForwardSpeed = runSpeed; // set max speed
+		stamina -= 0.5f;
+		if (stamina <= 0) {
+			noSprint = true;
+		}
+	}
+	else {
+		chMotor.movement.maxForwardSpeed = walkSpeed;
+		if (stamina < 100f) {
+			stamina += 0.5f;
+		}
+		if (stamina > 50) {
+			noSprint = false;
+		}
+	}
+}
