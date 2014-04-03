ceeddd9f484b37bbe95a72f429c7fcdacaead401
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-04-02 18:31:05 -0700

    Add stamina bar
    Add stamina bar to top left corner of screen. Stamina bar
    accurately displays player's current stamina.

--- Assets/Scripts/SprintController.js
+++ Assets/Scripts/SprintController.js
@@ -21,7 +21,10 @@ function Update () {
 	else {
 		chMotor.movement.maxForwardSpeed = walkSpeed;
 		if (stamina < 100f) {
-			stamina += 0.5f;
+			stamina += 0.3f;
+		}
+		if (stamina > 100f) {
+			stamina = 100f;
 		}
 		if (stamina > 50) {
 			noSprint = false;
---
+++ Assets/Scripts/StaminaBarController.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+private var maxStamina = 100f;
+private var currStamina = 100f;
+private var staminaBarLength;
+private var style : GUIStyle;
+private var texture : Texture2D;
+function Start () {
+	staminaBarLength = Screen.width / 2;
+	style = new GUIStyle();
+	texture = new Texture2D(128, 128);
+}
+
+function Update () {
+	currStamina = GetComponent(SprintController).stamina;
+	staminaBarLength = (Screen.width / 2) * (currStamina / maxStamina);
+}
+
+function OnGUI() {
+	texture.SetPixel(0, 0, Color.white);
+    texture.Apply();
+    style.normal.background = texture;
+    GUI.backgroundColor = new Color(0, 136, 0, 255);
+	GUI.Box(new Rect(10, 10, staminaBarLength, 20), "", style);
+	GUI.backgroundColor = Color.white;
+}
