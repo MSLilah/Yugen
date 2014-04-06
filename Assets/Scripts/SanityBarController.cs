ac6ef846102f7c013b6790a59f14d0ba7cb3e2c2
Hailee Kenney <hailee@puppetlabs.com>
2014-04-06 11:26:38 -0700

    Add sanity bar and update stamina bar
    Add preliminary sanity bar but not sanity effects. Update stamina
    bar to show in all scenes.

---
+++ Assets/Scripts/SanityBarController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SanityBarController : MonoBehaviour {
+	
+	private int maxSanity = 100;
+	private int currSanity = 100;
+	private int sanityBarLength;
+	private GUIStyle style;
+	private Texture2D texture;
+	
+	// Use this for initialization
+	void Start () {
+		sanityBarLength = Screen.width / 2;
+		style = new GUIStyle();
+		texture = new Texture2D(128, 128);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sanityBarLength = (Screen.width / 2) * (currSanity / maxSanity);
+	}
+	
+	void OnGUI() {
+		texture.SetPixel(0, 0, Color.white);
+		texture.Apply();
+		style.normal.background = texture;
+		GUI.backgroundColor = new Color(136, 0, 0, 255);
+		GUI.Box(new Rect(10, 40, sanityBarLength, 20), "", style);
+		GUI.backgroundColor = Color.white;
+	}
+}
