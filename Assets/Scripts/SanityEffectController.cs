6ab2c8d19991a610c7328dd33f14c4a70a6ec821
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-04-10 17:19:30 -0700

    Add "writing on wall" sanity effect
    Add sanity effect where wall texture will change to look like
    writing on the wall when sanity drops below 1/4 of its maximum
    value.

---
+++ Assets/Scripts/SanityEffectController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SanityEffectController : MonoBehaviour {
+	private SanityBarController sbc;
+	private Light light;
+	private Color insaneLight;
+
+	private bool writingDisplay = false;
+	private bool changeToWriting = true;
+
+	private GUIStyle style;
+	private Texture2D texture;
+
+	public Material writing;
+	public Material normalWall;
+	// Use this for initialization
+	void Start () {
+		sbc = GetComponent<SanityBarController>();
+		light = GameObject.FindGameObjectWithTag ("Light").light;
+		insaneLight = new Color32 (193, 101, 101, 255);
+		style = new GUIStyle();
+		texture = new Texture2D(128, 128);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sbc.currSanity < 50f) {
+			light.color = insaneLight;
+		}
+		else if (light.color != Color.white) {
+			light.color = Color.white;
+		}
+		if (sbc.currSanity < 25f && !writingDisplay) {
+			WritingOnTheWall(writing);
+			writingDisplay = true;
+		}
+		else if (sbc.currSanity > 25f && writingDisplay) {
+			WritingOnTheWall(normalWall);
+		}
+	}
+
+	void WritingOnTheWall(Material m) {
+		GameObject[] walls = GameObject.FindGameObjectsWithTag ("Wall");
+		for (int i = 0; i < walls.Length; i++) {
+			walls[i].renderer.material = m;
+		}
+	}
+}
