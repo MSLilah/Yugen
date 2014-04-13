d2d8a2286151ebdc60e192d028aeeaf6153b6e55
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-04-13 10:19:51 -0700

    Remove debug messages from PlayerInitialPlacementController

--- Assets/Scripts/PlayerInitialPlacementController.cs
+++ Assets/Scripts/PlayerInitialPlacementController.cs
@@ -8,9 +8,7 @@ public class PlayerInitialPlacementController : MonoBehaviour {
 	public static bool movePlayer = false;
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Initializing");
 		if (movePlayer) {
-			Debug.Log ("Placing player");
 			gameObject.transform.position = lastKnownPlayerPosition;
 			gameObject.transform.rotation = lastKnownPlayerRotation;
 			transform.eulerAngles = new Vector3(0,180,0);
