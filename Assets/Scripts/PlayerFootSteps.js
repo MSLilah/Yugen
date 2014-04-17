4789a845603d4edd96f8ee4f10f3f5aecd38961c
Sam Golloway <sgolloway14@gmail.com>
2014-04-16 23:18:26 -0700

    made fourth house scary?

---
+++ Assets/Scripts/PlayerFootSteps.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+var stepSound : AudioClip;
+private var chMotor : CharacterMotor;
+private var controller : CharacterController;
+private var stepTimer = 0.5;
+function Start () {
+	chMotor = GetComponent(CharacterMotor);
+	controller = GetComponent(CharacterController);
+}
+
+function Update () {
+
+	if( controller.velocity.magnitude > 0 )
+	{
+		StepSound();
+	}
+	
+	if(stepTimer > 0)
+	{
+		stepTimer -= Time.deltaTime;
+	}
+}
+function StepSound()
+{
+	if(stepTimer <= 0)
+	{
+		audio.PlayOneShot(stepSound);
+		stepTimer = 0.8;
+	}
+}
