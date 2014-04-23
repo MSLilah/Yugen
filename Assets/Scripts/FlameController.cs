cf073f3c39c0763be73ea54bdab44e3f7b155bf5
Janel Raab <raab14@up.edu>
2014-04-23 06:45:41 -0700

    A flame appears over a grave for every clue found

---
+++ Assets/Scripts/FlameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameController : MonoBehaviour {
+	public string clueKey;
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.GetInt (clueKey) != 1) 
+		{
+			Destroy(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	
+	}
+}
