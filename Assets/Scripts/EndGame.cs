6aabe79b80359e9c2ee7d8cb7cc0c4c902617298
Hailee Kenney <hailee@puppetlabs.com>
2014-04-23 18:12:54 -0700

    Add endgame scene

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width - 200, Screen.height- 150, 150, 75), "Return to Main Menu")) {
+			Application.LoadLevel ("Main Menu");
+		}
+	}
+}
